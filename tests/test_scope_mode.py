9339fbde36a85b6647b12241ad3d41d0c8bdb172
adminprivileges <caldwellthaddeus@protonmail.com>
2026-03-07 17:39:58 -0500

    added new tests

--- tests/test_scope_mode.py
+++ tests/test_scope_mode.py
@@ -1,6 +1,12 @@
 from ebph import defs
 from ebph.ebphd import parse_args
-from ebph.utils import compose_profile_key
+from ebph.utils import compose_profile_key, calculate_profile_key_from_stat, get_process_executable_key
+
+
+class _FakeStat:
+    def __init__(self, st_dev, st_ino):
+        self.st_dev = st_dev
+        self.st_ino = st_ino
 
 
 def test_parse_scope_mode_host():
@@ -21,3 +27,20 @@ def test_compose_profile_key_host_compatibility():
 def test_compose_profile_key_scope_sensitive():
     executable_key = 0xABCDEF
     assert compose_profile_key(1, executable_key) != compose_profile_key(2, executable_key)
+
+
+def test_calculate_profile_key_from_stat():
+    s = _FakeStat(12, 34)
+    assert calculate_profile_key_from_stat(s) == (12 << 32 | 34)
+
+
+def test_get_process_executable_key_fallback(monkeypatch):
+    import ebph.utils as utils
+
+    def fake_stat(path):
+        raise FileNotFoundError(path)
+
+    monkeypatch.setattr(utils.os, 'stat', fake_stat)
+    monkeypatch.setattr(utils, 'calculate_profile_key', lambda p: 0x1337)
+
+    assert get_process_executable_key(1234, '/fake/exe') == 0x1337
